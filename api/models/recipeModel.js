0cc95924bd11f04c29d46a210a7992d6964ea782
omta <omtadayma2001@gmail.com>
2024-04-02 15:38:41 +0530

    create recipe model

---
+++ api/models/recipeModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const recipeSchema = new mongoose.Schema({
+    title: {
+         type: String, required: true 
+    },
+    inst: {
+         type: String, required: true 
+    },
+    ing1:{type:String},
+    ing2:{type:String},
+    ing3:{type:String},
+    ing4:{type:String},
+    qty1:{type:String},
+    qty2:{type:String},
+    qty3:{type:String},
+    qty4:{type:String},
+    imgurl:{
+        type: String, required: true 
+   },
+   
+},{timestamps:true}); //this  will add createdAt and updatedAt to our schema
+
+const Recipe = mongoose.model( 'Recipe',recipeSchema );//creating a model called User with the schema defined above
+export default Recipe;
