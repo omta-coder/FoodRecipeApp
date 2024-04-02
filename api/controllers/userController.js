997824218b2e85bc15de2110ae67b41a5b00bd6c
omta <omtadayma2001@gmail.com>
2024-04-02 14:38:08 +0530

    first commit

---
+++ api/controllers/userController.js
@@ -0,0 +1,32 @@
+import User from '../models/userModel.js';
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import { errorHandler } from '../utils/error.js';
+
+export  const register = async (req, res,next) => {
+    const  {name,email,password} = req.body;
+    const hashedPassword = bcrypt.hashSync("password",10)
+    const newUser = new  User({name,email,password:hashedPassword});
+    try {
+        await  newUser.save()
+         res.status(201).json("newUser created");
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const login  = async (req,res,next)=>{
+   const{ email , password }= req.body ;
+   try {
+    const validUser = await User.findOne({email})
+    if(!validUser) return next(errorHandler(401, "User not found"))
+    const validPassword = bcrypt.compareSync("password",validUser.password);
+   if (!validPassword ) return next(errorHandler(401,'Invalid Password'));
+   const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+   const {password:hashedPassword,...rest} = validUser._doc;
+   const expiryDate = new Date(Date.now()+3600000);
+   res.cookie('access_token',token,{httpOnly:true,expires:expiryDate}).json({message:`Welcome ${validUser.name}`,rest})
+   } catch (error) {
+    next(error);
+   }
+}
---
+++ api/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import userRoute from './routes/userRoute.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = express();
+app.use(express.json());
+
+app.use('/api',userRoute)
+
+mongoose.connect(process.env.MONGODB)
+.then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) =>{
+    console.error(err);
+});
+
+app.listen(3000,(req,res)=>{
+    console.log("server run on port 3000");
+})
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode  || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success:false,
+        message,
+        statusCode
+    })
+})
