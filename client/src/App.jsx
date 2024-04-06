58cc9d3ef49ca637da8657431ede7e2b4d393333
omta <omtadayma2001@gmail.com>
2024-04-06 10:38:48 +0530

    create components route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import {BrowserRouter,Routes,Route} from  "react-router-dom";
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import Login from './components/Login';
+import Register from './components/Register';
+import Saved from './components/Saved';
+import Profile from './components/Profile';
+import AddRecipe from './components/AddRecipe';
+import Detail from './components/Detail';
 
 const App = () => {
   return (
@@ -9,6 +15,12 @@ const App = () => {
     <Navbar/>
     <Routes>
       <Route  path="/" element={<Home />}/>
+      <Route  path="/login" element={<Login />}/>
+      <Route  path="/register" element={<Register />}/>
+      <Route  path="profile" element={<Profile />}/>
+      <Route  path="/saved" element={<Saved />}/>
+      <Route  path="/add" element={<AddRecipe />}/>
+      <Route  path="/:id" element={<Detail />}/>
     </Routes>
     </BrowserRouter>
   )
