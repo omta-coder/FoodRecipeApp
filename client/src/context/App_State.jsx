02901cb11e1be424f2499f18e738f4e68466270e
omta <omtadayma2001@gmail.com>
2024-04-07 14:31:46 +0530

    connect frontend to backend

--- client/src/context/App_State.jsx
+++ client/src/context/App_State.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AppContext } from './App_Context'
+import axios  from "axios"
 
 
 const App_State = (props) => {
+  const url = "http://localhost:3000/api"
+
+  useEffect(() => {
+  login("nick1@gmail.com",123456)
+  }, [])
+  
+
+  //Login
+  const login = async(email,password)=>{
+    const  res= await axios.post(`${url}/login`,
+    { 
+      email , password 
+    },
+    {
+      headers:{
+        "Content-Type":"application/json"
+      },
+      withCredentials:true
+    });
+    console.log(res);
+  }
   return (
     <AppContext.Provider value={{}}>{props.children}</AppContext.Provider>
   )
