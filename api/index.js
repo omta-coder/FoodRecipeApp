6299e4db52097759999125c0cc8238ae00bb4d0e
omta <omtadayma2001@gmail.com>
2024-04-07 14:35:17 +0530

    add cors

--- api/index.js
+++ api/index.js
@@ -3,10 +3,16 @@ import mongoose from 'mongoose';
 import userRoute from './routes/userRoute.js'
 import recipeRoute from './routes/reciepeRoute.js'
 import dotenv from 'dotenv'
+import cors  from 'cors'
 dotenv.config()
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin:true,
+    methods:['GET','POST','PUT',"DELETE"],
+    credentials: true
+}))
 
 //user Route
 app.use('/api',userRoute)
