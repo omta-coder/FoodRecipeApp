56e93091f6d07f0b3478ac73eef52a9a71cb636e
omta <omtadayma2001@gmail.com>
2024-04-07 14:37:17 +0530

    changes

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -5,29 +5,29 @@ const Login = () => {
     <div className="container p-5 my-5" style={{width:"500px", border: "2px solid yellow",borderRadius: "10px"}}>
       <h2 className="text-center">Login</h2>
       <form>
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">
             Email address
           </label>
           <input
             type="email"
-            class="form-control"
+            className="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
           />
         </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">
+        <div className="mb-3">
+          <label for="exampleInputPassword1" className="form-label">
             Password
           </label>
           <input
             type="password"
-            class="form-control"
+            className="form-control"
             id="exampleInputPassword1"
           />
         </div>
         <div className="container d-grid col-6">
-        <button type="submit" class="btn btn-primary my-3">
+        <button type="submit" className="btn btn-primary my-3">
           Login
         </button>
         </div>
