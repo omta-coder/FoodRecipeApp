dc5546a497315f602ec84dbbe6a5ab502cfca7b8
omta <omtadayma2001@gmail.com>
2024-04-07 12:37:03 +0530

    create register page

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -2,7 +2,48 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className="container p-5 my-5" style={{width:"500px", border: "2px solid yellow",borderRadius: "10px"}}>
+    <h2 className="text-center">Register</h2>
+    <form>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">
+          Name
+        </label>
+        <input
+          type="name"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">
+          Email address
+        </label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+      <div className="container d-grid col-6">
+      <button type="submit" class="btn btn-primary my-3">
+        Register
+      </button>
+      </div>
+    </form>
+  </div>
   )
 }
 
